63571887e2d9fbeb5d7df5be346b34c51ff9ecc1
Jonathan Pokorny <j.s.pokorny@gmail.com>
2023-01-14 20:43:09 -0500

    uploading pypoll to github

---
+++ PyPoll/pypoll.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import csv
+import os
+
+
+
+#Files to load and output
+file_to_load = os.path.join(".", "Resources", "election_data.csv")
+file_to_output = os.path.join(".", "election_analysis.txt")
+
+#Total Vote Counter                       
+total_votes = 0
+
+
+#Candidate options and vote counters
+candidate_votes = {}
+candidate_options = []
+
+#Winning Candidate and winning count tracker
+winning_candidate = ""
+winning_count = 0
+
+with open(file_to_load) as election_data:
+    
+    # 
+    reader = csv.reader(election_data)
+    
+    # read the header
+    header = next(reader)
+    #print(header)
+    
+    for row in reader:
+        # add to the total vote count
+        total_votes = total_votes + 1
+        #print(row)
+        
+        # get candidate name from each row
+        candidate_name = row[2]
+        
+        # if candidate does not match any existing candidate
+        # in a way our loop is discovering candidates as it goes
+        
+        if candidate_name not in candidate_options:
+            
+            #add it to the list of candidates in the running
+            candidate_options.append(candidate_name)
+            
+            candidate_votes[candidate_name] = 0
+            
+        candidate_votes[candidate_name] += 1
+        
+            
+#print(candidate_votes)            
+#print(candidate_options)
+        
+with open(file_to_output, "w") as txt_file:       
+    election_results = (
+        f"Election Results\n"
+        f"-------------------------\n"
+        f"Total Votes {total_votes}\n"
+        f"-------------------------\n"
+    )
+    print(election_results, end = "")
+
+    txt_file.write(election_results)
+    
+    for candidate in candidate_votes:
+        
+        votes = candidate_votes[candidate]
+        vote_percentage = float(votes) / float(total_votes) * 100
+        
+        if(votes > winning_count):
+            winning_count = votes
+            winning_candidate = candidate
+            
+        voter_output = f"{candidate}: {vote_percentage:.3f}% ({votes})\n"
+        
+        print(voter_output, end = "")
+        
+        txt_file.write(voter_output)
+        
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"-------------------------"
+    )
+    print(winning_candidate_summary)
+    
+    txt_file.write(winning_candidate_summary)
+    
+    
+# Election Results
+# -------------------------
+# Total Votes: 369711
+# -------------------------
+# Charles Casper Stockham: 23.049% (85213)
+# Diana DeGette: 73.812% (272892)
+# Raymon Anthony Doane: 3.139% (11606)
+# -------------------------
+# Winner: Diana DeGette
+# -------------------------
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
